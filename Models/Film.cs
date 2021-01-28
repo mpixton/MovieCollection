6932ca81a771dbb139dc3247240ef03e5ce8c2b8
mpixton <71086131+mpixton@users.noreply.github.com>
2021-01-28 12:40:14 -0700

    FEAT: add film model
    Added Film model to the project.

---
+++ Models/Film.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieCollection.Models
+{
+    public class Film
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public DateTime Year { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public string Rating { get; set; }
+
+        public bool Edited { get; set; }
+
+        public string LentTo { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
